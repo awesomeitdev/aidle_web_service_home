8ddfa6d0cbdae49f8594324bfe9216a5b64100b2
ohjaeheon1 <148048031+ohjaeheon1@users.noreply.github.com>
2023-11-16 03:04:45 +0900

    Add files via upload

---
+++ user guide/scripts.js
@@ -0,0 +1,46 @@
+// 페이지 로딩 시 URL의 쿼리 파라미터에 따라 메뉴 아이템 활성화 및 콘텐츠 표시
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    if (id) {
+        const menuItem = document.querySelector(`#${id}`);
+
+        if (menuItem) {
+            menuItem.click(); // 해당 메뉴 아이템 클릭 이벤트 발생
+        }
+    }
+}
+
+document.querySelectorAll('#sidebar ul li').forEach(item => {
+    item.addEventListener('click', event => {
+        document.querySelectorAll('#sidebar ul li').forEach(item => {
+            item.classList.remove('active');
+        });
+
+        event.target.classList.add('active');
+
+        const target = event.target.getAttribute('data-target');
+
+        document.querySelectorAll('#content .content-item').forEach(item => {
+            item.style.display = 'none';
+        });
+
+        document.querySelector(`#${target}`).style.display = 'block';
+
+        // URL의 쿼리 파라미터 업데이트
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('id', event.target.getAttribute('id'));
+        window.history.replaceState({}, '', `${window.location.pathname}?${urlParams}`);
+    });
+});
+
+// 헤더버튼 효과
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector('.button');
+    const buttonList = document.querySelector('.button_list')
+
+    button.addEventListener('click', function () {
+        buttonList.classList.toggle('show_list');
+    })
+})
