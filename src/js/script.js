5d35d2cec084c0be69b60f775b42c51b7bf50e59
youngjung33 <youngj5396@awesomeit.co.kr>
2023-11-27 19:16:36 +0900

    innerWidth따라 변경

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,15 @@ function handleScroll(event) {
         if (currentSection == 1 && !isImageChanged) { //첫번째 섹션
             // 조건이 맞으면 backgroundImage변경
             document.getElementById('section1').style.backgroundImage = "url('./public/images/main_background2.png')";
+
+            if ((window.innerWidth - 150) >= 468) {
+                console.log(window.innerWidth);
+                document.getElementById('section1').style.backgroundImage = "url('./public/images/phone_2.png')";
+            } else if ((window.innerWidth - 150) < 468) {
+                console.log(window.innerWidth);
+                document.getElementById('section1').style.backgroundImage = "url('./public/images/section13_phone.png')";
+            }
+
             isImageChanged = true;
 
             // 헤더 텍스트와 로고 변경
@@ -175,7 +184,7 @@ function handleScroll(event) {
     } else if (event.deltaY < 0) { //휠 위로
         if (isImageChanged && currentSection == 1) {
             // 조건에 맞으면 배경이미지 변경
-            document.getElementById('section1').style.backgroundImage = "url('./public/images/main_background1.png')";
+            // document.getElementById('section1').style.backgroundImage = "url('./public/images/main_background1.png')";
             isImageChanged = false;
             // 헤더와 로고 변경
             if (!header.classList.contains('active')) {
@@ -184,6 +193,14 @@ function handleScroll(event) {
             headerText.forEach(text => {
                 text.classList.remove('background_white');
             });
+
+            if ((window.innerWidth - 150) >= 468) {
+                console.log(window.innerWidth);
+                document.getElementById('section1').style.backgroundImage = "url('./public/images/section13_phone.png')";
+            } else if ((window.innerWidth - 150) < 468) {
+                console.log(window.innerWidth);
+                document.getElementById('section1').style.backgroundImage = "url('./public/images/phone_2.png')";
+            }
 
             hamburgerMenu.style.backgroundImage = "url('./public/images/hamburger-icon.png')";
         } else if (currentSection > 1) {
@@ -656,10 +673,10 @@ document.querySelectorAll('#sidebar ul li').forEach(item => {
 
 document.addEventListener("DOMContentLoaded", function () {
     function updateSectionBackground() {
-        if (window.innerWidth >= 468) {
+        if ((window.innerWidth - 150) >= 468) {
             console.log(window.innerWidth);
             document.getElementById('section1').style.backgroundImage = "url('./public/images/main_background2.png')";
-        } else if (window.innerWidth < 468) {
+        } else if ((window.innerWidth - 150) < 468) {
             console.log(window.innerWidth);
             document.getElementById('section1').style.backgroundImage = "url('./public/images/main_background1.png')";
         }
